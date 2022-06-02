66b43edd4f12ab25ed74c3f36a756e8428af9efd
CBCastaldo <lady_lacrosse@hotmail.com>
2022-06-02 03:34:42 -0600

    Updated JSON and JSON.js

---
+++ scripts/json.js
@@ -0,0 +1,128 @@
+let bigneyList = [];
+
+const output = (people) => {
+    people.forEach(
+        person => {
+            let article = document.createElement('article');
+
+            let personName = document.createElement('h3');
+            personName.textContent = person.personName;
+
+            let birthDate = document.createElement('h4');
+            birthDate.textContent = person.birthDate;
+
+            let birthPlace = document.createElement('h4');
+            birthPlace.textContent = person.birthPlace;
+
+            let deathDate = document.createElement('h4');
+            deathDate.textContent = person.deathDate;
+
+            let deathPlace = document.createElement('h4');
+            deathPlace.textContent = person.deathPlace;
+
+            let img = document.createElement('img');
+            img.setAttribute('src', person.imageUrl);
+            img.setAttribute('alt', person.personName);
+
+            article.appendChild(personName);
+            article.appendChild(birthDate);
+            article.appendChild(birthPlace);
+            article.appendChild(deathDate);
+            article.appendChild(deathPlace);
+            article.appendChild(img);
+
+            document.querySelector('#people').appendChild(article);
+        }
+    );
+}
+
+const url = '';
+let results = null;
+
+async function getPeople(url) {
+    const response = await fetch(url);
+    if (response.ok) {
+        const data = await response.json();
+        output(data);
+    }
+};
+
+const reset = () => {
+    document.querySelector('#people').innerHTML = '';
+}
+
+const sortBy = () => {
+    reset();
+
+    let filter = document.querySelector('#sortBy').value;
+
+    switch (filter) {
+        case 'NameAscending':
+            output(bigneyList.sort(
+                (person1, person2) => {
+                    let personName1 = person1.personName.toLowerCase();
+                    let personName2 = person2.personName.toLowerCase();
+                    if (personName1 < personName2) return -1;
+                    else if (personName1 > personName2) return 1;
+                    else return 0;
+                }));
+            break;
+        case 'NameDescending':
+            output(bigneyList.sort(
+                (person1, person2) => {
+                    let personName1 = person1.personName.toLowerCase();
+                    let personName2 = person2.personName.toLowerCase();
+                    if (personName1 > personName2) return -1;
+                    else if (personName1 < personName2) return 1;
+                    else return 0;
+                }));
+            break;
+            case 'BirthDateAscending':
+            output(bigneyList.sort(
+                (person1, person2) => {
+                    let personbirthDate1 = person1.personbirthDate;
+                    let personbirthDate2 = person2.personbirthDate;
+                    if (personbirthDate1 < personbirthDate2) return -1;
+                    else if (personbirthDate1 > personbirthDate2) return 1;
+                    else return 0;
+                }));
+            break;
+        case 'BirthDateDescending':
+            output(bigneyList.sort(
+                (person1, person2) => {
+                    let personbirthDate1 = person1.personbirthDate;
+                    let personbirthDate2 = person2.personbirthDate;
+                    if (personbirthDate1 > personbirthDate2) return -1;
+                    else if (personbirthDate1 < personbirthDate2) return 1;
+                    else return 0;
+                }));
+            break;
+            case 'DeathDateAscending':
+            output(bigneyList.sort(
+                (person1, person2) => {
+                    let persondeathDate1 = person1.persondeathDate;
+                    let persondeathDate2 = person2.persondeathDate;
+                    if (persondeathDate1 < persondeathDate2) return -1;
+                    else if (persondeathDate1 > persondeathDate2) return 1;
+                    else return 0;
+                }));
+            break;
+        case 'DeathDateDescending':
+            output(bigneyList.sort(
+                (person1, person2) => {
+                    let persondeathDate1 = person1.persondeathDate;
+                    let persondeathDate2 = person2.persondeathDate;
+                    if (persondeathDate1 > persondeathDate2) return -1;
+                    else if (persondeathDate1 < persondeathDate2) return 1;
+                    else return 0;
+                }));
+            break;
+        default:
+            // using ternary operators
+            output(bigneyList.sort(
+                (person1, person2) => 
+                person1.personName.toLowerCase() > person2.personName.toLowerCase() ? 1 : 
+                    person2.personName.toLowerCase() > person1.personName.toLowerCase() ? -1 : 0));
+            break;
+    }
+}
